3b28f154370ead4d786d3fbd643320a804b0cba0
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-07 01:27:35 +0100

    Created Movie Class that allows user to check for Movie information.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import java.lang.management.MonitorInfo;
+
 public class Main {
+
     public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.getMovieDetails();
 
     }
 }
---
+++ src/main/java/Movie.java
@@ -0,0 +1,15 @@
+public class Movie {
+
+    public void getMovieDetails(){
+
+        String nameOfTheMovie = "\"Alien vs. Predator\"";
+        String nameOfTheDirectors = "Paul W.S. Anderson, Colin Strause, Greg Strause";
+        int durationInMinutes = 101;
+        int releaseDate = 2004;
+
+        System.out.println("Movie: " + nameOfTheMovie + "\n" + "Directory: " + nameOfTheDirectors +
+                "\n" + "Movie duration: " + durationInMinutes + " minutes" + "\n" + "Release date: "+ releaseDate);
+
+    }
+
+}
