8cdd45785c07a6bbca2ea2a7bdb5bc974047f0de
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-11 21:24:26 +0100

    Added unit test for last method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
+
     public static boolean isItRectangularTriangle(int sideA, int sideB, int sideC){
-        if((sideA*sideA)+(sideB*sideB)==(sideC*sideC)){
+        if((sideA * sideA) + (sideB * sideB) == (sideC * sideC)){
             return true;
         }else
             return false;
