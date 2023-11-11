11c81efd408958c37e0d83949252699c40393706
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-11 20:46:20 +0100

    Added method for checking if triangle is rectangular

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
-
+    public static boolean isItRectangularTriangle(int sideA, int sideB, int sideC){
+        if((sideA*sideA)+(sideB*sideB)==(sideC*sideC)){
+            return true;
+        }else
+            return false;
     }
 }
